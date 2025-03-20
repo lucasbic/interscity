dbc56ed8b8ce252353480d5c82fcb42f137a2b56
Lucas Bicalho <lucas@LUCAS-PC>
2025-03-20 18:25:56 -0300

    add sqs

--- src/data-collector/config/initializers/workers.rb
+++ src/data-collector/config/initializers/workers.rb
@@ -1,13 +1,24 @@
 if Rails.env.development? || Rails.env.production?
-  require 'bunny'
+  #require 'bunny'
+  require 'aws-sdk-sqs'
 
   WORKERS_LOGGER ||= Logger.new("#{Rails.root}/log/workers.log")
 
-  Rails.configuration.worker_conn = Bunny.new(
-    hostname: SERVICES_CONFIG['services']['rabbitmq'],
-    logger: WORKERS_LOGGER,
+  # Rails.configuration.worker_conn = Bunny.new(
+  #   hostname: SERVICES_CONFIG['services']['rabbitmq'],
+  #   logger: WORKERS_LOGGER,
+  # )
+  # Rails.configuration.worker_conn.start
+
+  # Configurar o cliente SQS
+  Rails.configuration.sqs_client = Aws::SQS::Client.new(
+    region: 'sa-east-1',
+    access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+    secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
   )
-  Rails.configuration.worker_conn.start
+
+  # URL da fila SQS
+  Rails.configuration.sqs_queue_url = ENV['SQS_QUEUE_URL']
 
   resource_creator_worker = ResourceCreator.new(2, 2)
   resource_creator_worker.perform
